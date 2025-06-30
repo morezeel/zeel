9d097ae397e39f36ee34dc424d027ed3ca6fd97a
morezeel <morezeel579@gmail.com>
2025-06-30 12:48:39 +0530

    Add files via upload

---
+++ L-9.py
@@ -0,0 +1,41 @@
+#  String Formatting
+
+#  String Value using f character
+
+name = "John"
+age = 30
+
+profile = "my name is {} and I am {} years old".format(name, age)
+print(profile)
+profile_f = f"my name is {name} and I am {age} years old"
+
+print(profile)
+print("my name is {} and my age is {}".format(name, age))
+
+# String with indexing and slicing 
+
+statement = "Hello Python"
+print(statement[-2])
+print(statement[0])  
+print(statement[1])
+print(statement[2])
+print(statement[3])
+print(statement[4])
+print(statement[5])                
+
+print(statement[6])
+print(statement[7])
+print(statement[8])
+print(statement[9])
+print(statement[10])
+print(statement[11])
+
+#  list 
+
+lists = [10 , 20 , 30]
+lists = [10 , 20 , "hello"]
+
+print(lists)
+print(lists[0])
+print(lists[1])
+print(lists[2])
